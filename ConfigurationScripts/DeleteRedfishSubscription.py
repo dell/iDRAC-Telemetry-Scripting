d8e84635cf1bfb916be1708016fb2ff8a5589e61
Sankunny Jayaprasad <sankunny.jayaprasad@delllabs.net>
2020-07-14 14:20:03 -0500

    Adding option to delete all subscriptions.

--- ConfigurationScripts/DeleteRedfishSubscription.py
+++ ConfigurationScripts/DeleteRedfishSubscription.py
@@ -4,7 +4,7 @@
 #
 #
 # _author_ = Sankunny Jayaprasad <Sankunny.Jayaprasad@Dell.com>
-# _version_ = 1.0
+# _version_ = 1.1
 #
 # Copyright (c) 2020, Dell, Inc.
 #
@@ -35,8 +35,9 @@ parser.add_argument('script_examples', action = "store_true",
                     help = "'python DeleteRedfishSubscription.py -ip 192.168.0.120 -u root -p calvin -v y' to view the subscriptions "
                            "'python DeleteRedfishSubscription.py -ip 192.168.0.120 -u root -p calvin -s e181b9a2-eaf6-11e9-94dc-f48e38cf169c' "
                            "to delete subscription with id e181b9a2-eaf6-11e9-94dc-f48e38cf169c" )
-parser.add_argument('-v',help = 'To view the existing subscription/s pass in \'y\'. If this option is added, -s option to delete subscription will be ignored',default = '', required = False)
-parser.add_argument('-s',help = 'The subscription ID to be deleted',required = False)
+parser.add_argument('-v',help = 'To view the existing subscription/s', action='store_true')
+parser.add_argument('-d',help = 'The subscription ID to be deleted',required = False)
+parser.add_argument('-a',help = 'Delete all subscriptions', action = "store_true")
 
 args = vars(parser.parse_args())
 
@@ -48,18 +49,24 @@ headers = {'content-type': 'application/json'}
 def validate_telemetry_support():
     url = 'https://{}/redfish/v1/TelemetryService'.format(idrac_ip)
     headers = {'content-type': 'application/json'}
-    response = requests.get(url, headers = headers, verify = False,auth = (idrac_username, idrac_password))
-    if response.status_code != 200:
-        logging.warning("Script can not be executed as Datacenter license is not installed or iDRAC firmware does not support Telemetry.")
-        logging.warning(json.loads(response.text).get("error",{}).get("@Message.ExtendedInfo",[{}])[0].get("Message",""))
+    try:
+        response = requests.get(url, headers = headers, verify = False,auth = (idrac_username, idrac_password))
+        if response.status_code != 200:
+            logging.warning("Script can not be executed as Datacenter license is not installed or iDRAC firmware does not support Telemetry.")
+            logging.warning(json.loads(response.text).get("error",{}).get("@Message.ExtendedInfo",[{}])[0].get("Message",""))
+            sys.exit(0)
+    except Exception as e:
+        logging.error("Unable to connect to the IP provided.")
         sys.exit(0)
 
 def log_subscription_details(subscriptions):
     logging.info("The active subscriptions are ".center(100,"*"))
+    subscription_ids = []
     for subscription in subscriptions:
         response = requests.get('https://{}{}'.format(idrac_ip,subscription.get("@odata.id","")), headers = headers, verify = False, auth = (idrac_username, idrac_password))
         if response.status_code == 200:
             response_date = json.loads(response.text)
+            subscription_ids.append(response_date.get("Id",""))
             logging.info("Context ID: {} , Destination : {}, ID : {}".format(response_date.get("Context"),response_date.get("Destination"),response_date.get("Id")))
         else:
             logging.error(
@@ -68,6 +75,7 @@ def log_subscription_details(subscriptions):
                 logging.error("FAIL, The response is: {}".format(response.text))
             sys.exit()
     logging.info("".center(100, "*"))
+    return subscription_ids
 
 def view_subscriptions():
     if args["v"]:
@@ -76,31 +84,42 @@ def view_subscriptions():
         if response.status_code == 200:
             response_date = json.loads(response.text)
             subscriptions = response_date.get("Members")
-            log_subscription_details(subscriptions)
+            return log_subscription_details(subscriptions)
         else:
             logging.error(
                 "FAIL, status code for reading subscriptions is not 200, code is: {}".format(response.status_code))
             if hasattr(response, 'text'):
                 logging.error("FAIL, The response is: {}".format(response.text))
+
+def delete_subscription(subscription_id):
+    logging.info("Attempting to delete subscription with ID : {}".format(subscription_id))
+    url = 'https://{}/redfish/v1/EventService/Subscriptions/{}'.format(idrac_ip,subscription_id)
+    response = requests.delete(url, headers = headers, verify = False, auth = (idrac_username, idrac_password))
+    if response.status_code == 200:
+        logging.info("Successfully deleted subscription with ID : {}".format(subscription_id))
+    else:
+        logging.error(
+            "FAIL, status code for deleting subscription is not 200, code is: {}".format(response.status_code))
+        if hasattr(response, 'text'):
+            logging.error("FAIL, The response is: {}".format(response.text))
+    sys.exit()
+
+def delete_subscriptions():
+    if args["a"]:
+        args.update({'v':True})
+        subscription_ids = view_subscriptions()
+        list(map(delete_subscription,subscription_ids))
         sys.exit()
-def delete_subscription():
-    if args["s"]:
-        subscription_id = args["s"]
-        logging.info("Attempting to delete subscription with ID : {}".format(subscription_id))
-        url = 'https://{}/redfish/v1/EventService/Subscriptions/{}'.format(idrac_ip,subscription_id)
-        response = requests.delete(url, headers = headers, verify = False, auth = (idrac_username, idrac_password))
-        if response.status_code == 200:
-            logging.info("Successfully deleted subscription with ID : {}".format(subscription_id))
-        else:
-            logging.error(
-                "FAIL, status code for deleting subscription is not 200, code is: {}".format(response.status_code))
-            if hasattr(response, 'text'):
-                logging.error("FAIL, The response is: {}".format(response.text))
+    elif args["d"]:
+        subscription_id = args["d"]
+        delete_subscription(subscription_id)
         sys.exit()
+
 if __name__ == "__main__":
-    validate_telemetry_support()
-    view_subscriptions()
-    delete_subscription()
-    if not (args["s"] and args["v"]):
-        logging.error("No valid options selected to run the script. Execute the script with -v or -s options")
+    if not (args["v"] or (args["a"] or args["d"] )):
+        logging.error("No valid options selected to run the script. Execute the script with -v or -d  or -a options")
         sys.exit(0)
+    validate_telemetry_support()
+    delete_subscriptions()
+    view_subscriptions()
+    sys.exit(0)
