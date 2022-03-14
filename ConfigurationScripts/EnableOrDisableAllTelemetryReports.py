dd4513fadf1736428dd8ba92211ec5f340e62eac
texroemer <30351677+texroemer@users.noreply.github.com>
2022-03-14 11:06:20 -0500

    Added CSV file support
    Added CSV file support to configure multiple iDRACs. Script now supports configuring one iDRAC using arguments or multiple iDRACs using CSV file.

--- ConfigurationScripts/EnableOrDisableAllTelemetryReports.py
+++ ConfigurationScripts/EnableOrDisableAllTelemetryReports.py
@@ -3,9 +3,9 @@
 # with Default/Existing settings.
 #
 #
-#
 # _author_ = Sankunny Jayaprasad <Sankunny.Jayaprasad@Dell.com>
-# _version_ = 1.0
+# _author_ = Texas Roemer <Texas_Roemer@Dell.com>
+# _version_ = 2.0
 #
 # Copyright (c) 2022, Dell, Inc.
 #
@@ -18,6 +18,7 @@
 #
 
 import argparse
+import csv
 import json
 import logging
 import sys
@@ -25,54 +26,57 @@ import warnings
 import requests
 
 warnings.filterwarnings("ignore")
-logging.getLogger().setLevel(logging.INFO)  # Change to logging.DEBUG for detailed logs
+#logging.getLogger().setLevel(logging.INFO)  # Change to logging.DEBUG for detailed logs
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-parser = argparse.ArgumentParser(description="Python script using Redfish to Enable/Disable all iDRAC Telemetry "
-                                             "reports. No other configuration changes are made.")
-parser.add_argument('-ip', help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('script_examples', action="store_true",
-                    help="'EnableOrDisableAllTelemetryReports.py -ip 192.168.0.120 -u root -p calvin -s Enabled' to enable all reports"
-                         "'EnableOrDisableAllTelemetryReports.py -ip 192.168.0.120 -u root -p calvin -s Disabled to disable all scripts'")
-parser.add_argument('-s',
-                    help='Pass in the report status to be set. Possible values are Enabled/Disabled', default='Enabled',
-                    required=False)
+parser = argparse.ArgumentParser(description="Python script using Redfish to Enable/Disable iDRAC Telemetry and all supported metric reports for one iDRAC using script arguments or multiple iDRACs using CSV file.")
+parser.add_argument('--script-examples', action="store_true", help='Prints script examples')
+parser.add_argument('-ip', help='iDRAC IP address, argument only required if configuring one iDRAC', required=False)
+parser.add_argument('-u', help='iDRAC username, argument only required if configuring one iDRAC', required=False)
+parser.add_argument('-p', help='iDRAC password, argument only required if configuring one iDRAC', required=False)
+parser.add_argument('-s', help='Pass in the report status to be set. Possible values are Enabled/Disabled', default='Enabled', required=False)
+parser.add_argument('-f', help='Pass in csv file name. If file is not located in same directory as script, pass in the full directory path with file name', required=False)
 
 args = vars(parser.parse_args())
 
-idrac_ip = args["ip"]
-idrac_username = args["u"]
-idrac_password = args["p"]
+def print_examples():
+    """
+    Print program examples and exit
+    """
+    print(
+        '\n\'EnableOrDisableAllTelemetryReports.py -ip 192.168.0.120 -u root -p calvin -s Enabled, this example will enable Telemetry and all metric reports for single iDRAC\n'
+        '\n\'EnableOrDisableAllTelemetryReports.py -ip 192.168.0.120 -u root -p calvin -s Disabled, this example will disable Telemetry and all metric reports for single iDRAC\n'
+        '\n\'EnableOrDisableAllTelemetryReports.py -ip 192.168.0.120 -u root -p calvin -s Enabled -f C:\Python39\iDRACs.csv, this example will enable Telemetry and all metric reports for all iDRACs in CSV file.\n'
+        '\n\'EnableOrDisableAllTelemetryReports.py -ip 192.168.0.120 -u root -p calvin -s Disabled -f C:\Python39\iDRACs.csv, this example will disable Telemetry and all metric reports for all iDRACs in CSV file.\n')
 
-
-def get_attributes():
+def get_attributes(ip, user, pwd):
     """ Checks the current status of telemetry and returns it
 
     Returns: A dictionary containing the attributes of the current telemetry status
     """
-    url = 'https://{}/redfish/v1/Managers/iDRAC.Embedded.1/Attributes'.format(idrac_ip)
+    global telemetry_attributes
+    url = 'https://{}/redfish/v1/Managers/iDRAC.Embedded.1/Attributes'.format(ip)
     headers = {'content-type': 'application/json'}
-    response = requests.get(url, headers=headers, verify=False, auth=(idrac_username, idrac_password))
+    response = requests.get(url, headers=headers, verify=False, auth=(user, pwd))
     if response.status_code != 200:
-        logging.error("FAIL, status code for reading attributes is not 200, code is: {}".format(response.status_code))
+        logging.error("- FAIL, status code for reading attributes is not 200, code is: {}".format(response.status_code))
         sys.exit()
     try:
-        logging.info("Successfully pulled configuration attributes")
+        logging.info("- INFO, successfully pulled configuration attributes")
         configurations_dict = json.loads(response.text)
         attributes = configurations_dict.get('Attributes', {})
         telemetry_attributes = {k: v for k, v in attributes.items() if
                                 (k.startswith('Telemetry')) and (k.endswith("EnableTelemetry"))}
-        logging.info("iDRAC Telemetry is currently '{}'.".format(
+        logging.info("- INFO, current iDRAC Telemetry global setting: '{}'".format(
             telemetry_attributes.get('Telemetry.1.EnableTelemetry', 'Unknown')))
         logging.debug(telemetry_attributes)
     except Exception as e:
-        logging.error("FAIL: detailed error message: {0}".format(e))
+        logging.error("- FAIL: detailed error message: {0}".format(e))
         sys.exit()
-    return telemetry_attributes
+    #return telemetry_attributes
 
 
-def set_attributes(telemetry_attributes):
+def set_attributes(ip, user, pwd):
     """Uses the RedFish API to set the telemetry enabled attribute to true.
 
     Args:
@@ -83,17 +87,35 @@ def set_attributes(telemetry_attributes):
     if status_to_set not in ['Enabled', 'Disabled']:
         logging.error("Invalid value for report status. Supported values are Enabled & Disabled")
         sys.exit()
-    url = 'https://{}/redfish/v1/Managers/iDRAC.Embedded.1/Attributes'.format(idrac_ip)
+    url = 'https://{}/redfish/v1/Managers/iDRAC.Embedded.1/Attributes'.format(ip)
     headers = {'content-type': 'application/json'}
     updated_telemetry_attributes = {k: status_to_set for k in telemetry_attributes.keys()}
     response = requests.patch(url, data=json.dumps({"Attributes": updated_telemetry_attributes}), headers=headers,
-                              verify=False, auth=(idrac_username, idrac_password))
+                              verify=False, auth=(user, pwd))
     if response.status_code != 200:
-        logging.error("FAIL, status code for reading attributes is not 200, code is: {}".format(response.status_code))
+        logging.error("- FAIL, status code for reading attributes is not 200, code is: {}".format(response.status_code))
         logging.debug(str(response))
         sys.exit()
-    logging.info("Successfully '{}' iDRAC Telemetry and all reports.".format(status_to_set))
+    logging.info("- INFO, successfully '{}' iDRAC Telemetry and all supported metric reports".format(status_to_set))
 
 
 if __name__ == "__main__":
-    set_attributes(get_attributes())
+    if args["script_examples"]:
+        print_examples()
+    elif args["ip"] and args["u"] and args["p"] and args["s"]:
+        get_attributes(args["ip"], args["u"], args["p"])
+        set_attributes(args["ip"], args["u"], args["p"])
+    elif args["s"] and args["f"] and args["s"]:
+        try:
+            open_csv_file = open(args["f"], encoding='UTF8')
+        except:
+            logging.error("\n- ERROR, unable to locate file %s" % args["f"])
+            sys.exit(0)
+        csv_reader = csv.reader(open_csv_file)
+        next(csv_reader)
+        for line in csv_reader:
+            logging.info("\n- %s Telemetry attributes for iDRAC %s -\n" % (args["s"], line[0]))
+            get_attributes(line[0], line[1], line[2])
+            set_attributes(line[0], line[1], line[2])
+    else:
+        logging.warning("- WARNING, missing or incorrect arguments passed in for executing script")
